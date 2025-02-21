4fa10e2459f16e52923a8c700b1bc7ace8ed0bab
HrackGG <contagogle000000g@gmail.com>
2025-02-21 18:00:42 -0300

    Atualizar o ScriptTeste.lua

--- ScriptTeste.lua
+++ ScriptTeste.lua
@@ -1,13 +1,19 @@
-gg.setRanges(gg.REGION_ANONYMOUS) -- Define a região da memória a ser escaneada  
-local results = gg.getResults(1000) -- Obtém os primeiros 1000 valores encontrados  
-local file = io.open("/storage/emulated/0/Download/memory_dump.txt", "w") -- Abre um arquivo para salvar o dump  
+gg.setRanges(gg.REGION_ANONYMOUS) -- Define a região de memória a ser escaneada  
+gg.searchNumber("0~999999999", gg.TYPE_DWORD) -- Pesquisa por valores inteiros (DWORD)  
+local results = gg.getResults(1000) -- Obtém os primeiros 1000 resultados encontrados  
 
-if file then
-    for i, v in ipairs(results) do
-        file:write(string.format("Endereço: %X | Valor: %s\n", v.address, v.value))
-    end
-    file:close()
-    gg.toast("Dump de memória salvo em Download/memory_dump.txt")  
+if #results == 0 then
+    gg.toast("Nenhum valor encontrado!")  
 else
-    gg.toast("Erro ao salvar o arquivo!")  
+    local file = io.open("/storage/emulated/0/Download/memory_dump.txt", "w") -- Abre um arquivo para salvar o dump  
+
+    if file then
+        for i, v in ipairs(results) do
+            file:write(string.format("Endereço: %X | Valor: %s\n", v.address, v.value))
+        end
+        file:close()
+        gg.toast("Dump de memória salvo em Download/memory_dump.txt")  
+    else
+        gg.toast("Erro ao salvar o arquivo!")  
+    end
 end
