2bec846c40ed2fbff4854321fbc84919cebce6c4
HrackGG <contagogle000000g@gmail.com>
2025-02-22 16:40:20 -0300

    Atualizar o ScriptTool.lua

--- ScriptTool.lua
+++ ScriptTool.lua
@@ -1,42 +1,28 @@
--- Verifica se o dispositivo está com root
-local rootCheck = io.popen("which su")
-local root = rootCheck:read("*l")
-rootCheck:close()
+-- Solicita o nome do pacote
+local packageName = "com.dts.freefireth"  -- Nome do pacote do app/jogo
+local address = "0x000000"  -- Endereço de memória onde o valor será modificado
+local newValue = 0  -- Novo valor a ser definido no endereço
 
-if not root then
-    gg.alert("Erro: O dispositivo precisa ser rootado para executar esse script!")
+-- Tenta identificar o pacote em execução
+local targetProcess = gg.getTargetInfo()
+if not targetProcess or targetProcess.packageName ~= packageName then
+    gg.alert("Erro: O pacote '" .. packageName .. "' não está rodando!")
     os.exit()
 end
 
--- Define a variável do pacote que queremos modificar
-local packageName = gg.prompt({"Digite o nome do pacote do jogo/aplicativo:"}, {""}, {"text"})
+-- Define o processo alvo para o GameGuardian
+gg.setTargetPackage(packageName)
 
-if not packageName or packageName[1] == "" then
-    gg.alert("Erro: Nenhum nome de pacote inserido!")
+-- Acessa diretamente o endereço de memória
+local results = gg.getResults(1)
+if #results == 0 then
+    gg.alert("Erro: Endereço de memória '" .. address .. "' não encontrado!")
     os.exit()
 end
 
--- Usando comando root para procurar o PID do processo
-local pidCommand = "ps | grep " .. packageName[1]
-local pid = io.popen(pidCommand):read("*l")
+-- Modifica o valor no endereço específico
+results[1].value = newValue
+gg.setValues(results)
 
-if not pid then
-    gg.alert("Erro: O pacote '" .. packageName[1] .. "' não está rodando!")
-    os.exit()
-end
-
--- Exibe o PID encontrado
-gg.alert("Processo encontrado! PID: " .. pid)
-
--- Comando para localizar o valor na memória
-local searchCommand = "cat /proc/" .. pid .. "/maps"
-local mapsData = io.popen(searchCommand):read("*all")
-
--- Aqui você poderia buscar por áreas de memória específicas e valores
--- Por exemplo, modificar valores em regiões específicas da memória do processo
-local modifyCommand = "echo 'NovoValor' > /proc/" .. pid .. "/mem"
-
--- Exemplo de como enviar um comando para modificar a memória (supondo que o valor tenha sido encontrado)
-os.execute(modifyCommand)
-
-gg.alert("Valor modificado com sucesso!")
+-- Mensagem de confirmação
+gg.alert("Valor modificado com sucesso no endereço: " .. address)
