dacdfbfecc487ebfe96333c1e54a84ed9d117315
HrackGG <contagogle000000g@gmail.com>
2025-02-22 17:40:46 -0300

    Atualizar o ScriptTool.lua

--- ScriptTool.lua
+++ ScriptTool.lua
@@ -1,3 +1,6 @@
+-- Requer a instalação do IUP
+require("iuplua")
+
 -- Função para criptografar
 function encrypt(text, shift)
     local encrypted = ""
@@ -22,30 +25,67 @@ function decrypt(encryptedText, shift)
     return decrypted
 end
 
--- Função principal para selecionar o que fazer
-function main()
-    print("Escolha uma opção: ")
-    print("1. Criptografar")
-    print("2. Descriptografar")
-    
-    local choice = io.read()  -- Lê a escolha do usuário
-    
-    print("Digite o texto:")
-    local inputText = io.read()  -- Lê o texto a ser processado
-    
-    print("Digite o valor do deslocamento (shift):")
-    local shift = tonumber(io.read())  -- Lê o valor do deslocamento (convertido para número)
-
-    if choice == "1" then
-        local encryptedText = encrypt(inputText, shift)
-        print("Texto criptografado: " .. encryptedText)
-    elseif choice == "2" then
-        local decryptedText = decrypt(inputText, shift)
-        print("Texto descriptografado: " .. decryptedText)
-    else
-        print("Opção inválida!")
+-- Função para exibir os códigos numéricos
+function showNumericCode(text)
+    local numericCode = ""
+    for i = 1, #text do
+        local num = string.byte(text:sub(i, i))
+        numericCode = numericCode .. num .. " "
     end
+    return numericCode
 end
 
--- Executa o programa
-main()
+-- Função chamada ao pressionar o botão "Criptografar"
+function encryptCallback()
+    local inputText = iup.GetText(textInput)  -- Obtém o texto inserido
+    local shift = tonumber(iup.GetText(shiftInput))  -- Obtém o valor de deslocamento
+    local encryptedText = encrypt(inputText, shift)
+    local numericEncrypted = showNumericCode(encryptedText)
+
+    -- Atualiza o campo de saída com o texto criptografado e código numérico
+    iup.SetText(outputText, "Texto criptografado: " .. encryptedText .. "\nCódigo numérico: " .. numericEncrypted)
+end
+
+-- Função chamada ao pressionar o botão "Descriptografar"
+function decryptCallback()
+    local inputText = iup.GetText(textInput)  -- Obtém o texto inserido
+    local shift = tonumber(iup.GetText(shiftInput))  -- Obtém o valor de deslocamento
+    local decryptedText = decrypt(inputText, shift)
+    local numericDecrypted = showNumericCode(decryptedText)
+
+    -- Atualiza o campo de saída com o texto descriptografado e código numérico
+    iup.SetText(outputText, "Texto descriptografado: " .. decryptedText .. "\nCódigo numérico: " .. numericDecrypted)
+end
+
+-- Cria a interface gráfica
+textInput = iup.text{size = "200x20", value = "Digite o texto aqui"}  -- Campo para inserir texto
+shiftInput = iup.text{size = "50x20", value = "3"}  -- Campo para inserir o deslocamento
+outputText = iup.text{size = "400x200", multiline = "YES"}  -- Campo de saída (criptografado/descriptografado)
+
+-- Botões
+encryptButton = iup.button{title = "Criptografar", action = encryptCallback}  -- Botão de criptografar
+decryptButton = iup.button{title = "Descriptografar", action = decryptCallback}  -- Botão de descriptografar
+
+-- Organiza os elementos da interface gráfica
+dlg = iup.dialog{
+    iup.vbox{
+        iup.label{title = "Texto original:"},
+        textInput,
+        iup.label{title = "Deslocamento (Shift):"},
+        shiftInput,
+        encryptButton,
+        decryptButton,
+        iup.label{title = "Resultado:"},
+        outputText
+    },
+    title = "Criptografador e Descriptografador",
+    size = "300x400"
+}
+
+-- Exibe a janela
+dlg:show()
+
+-- Loop da interface gráfica
+if (iup.MainLoopLevel() == 0) then
+    iup.MainLoop()
+end
